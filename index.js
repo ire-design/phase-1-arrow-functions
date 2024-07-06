ab417b6fdc34f954965cf24d0d3d07a6293df322
ire-design <irenemwikalii04@gmail.com>
2024-07-06 09:04:54 +0300

    complete

---
+++ index.js
@@ -0,0 +1,11 @@
+let par1 = 2000
+let par2 = 100
+
+const divide = function(par1, par2){
+    return par1 / par2
+     
+}
+
+const square = par1 => par1 * par1
+
+const add = (par1, par2) => par1 + par2
